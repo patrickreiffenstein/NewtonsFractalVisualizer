d56fe855dc5b2e2688babd12da8166ee2894434e
PayThePro <paythepro@gmail.com>
2021-12-07 04:16:01 +0100

    Bitmap and color foundation
    :)

---
+++ Bitmap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonsFractalVisualizer
+{
+	class Bitmap
+	{
+		public Color[] bits;
+		
+		public Bitmap(int height, int width)
+		{
+			bits = new Color[height * width];
+		}
+	}
+}
---
+++ Color.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonsFractalVisualizer
+{
+	class Color
+	{
+		public byte redValue, greenValue, blueValue;
+
+		/// <summary>
+		/// Color constructer, input the amount of red, green and blue a color should have.
+		/// </summary>
+		/// <param name="red"></param>
+		/// <param name="green"></param>
+		/// <param name="blue"></param>
+		public Color(byte red, byte green, byte blue)
+		{
+			redValue = red;
+			greenValue = green;
+			blueValue = blue;
+		}
+	}
+}
